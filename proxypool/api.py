a3c1f738658bd607766fc7d490ecacbb9351812c
wenfengand <wenfengand@126.com>
2018-12-08 14:02:05 +0800

    Add batch proxies query support. Add json support

--- proxypool/api.py
+++ proxypool/api.py
@@ -48,13 +48,29 @@ def get_proxy():
     :return: 随机代理
     """
     conn = get_conn()
-    return conn.random() 
+    return conn.random()[0] 
 @app.route('/v1/proxy')
 @token_required(API_TOKEN,'json')
 def get_proxy_v1():
     num = request.args.get('num') 
+
+    if num == None:
+        num = 1 
+    num = int(num) 
+
+    if num > 100:
+        num = 100 
+
+    if num < 0:
+        num = 1
     conn = get_conn() 
-    return conn.random() 
+    proxies = conn.random(num = num) 
+    
+    ret_dict = {} 
+    ret_dict['msg'] = 'ok' 
+    ret_dict['proxies'] = proxies 
+
+    return json.dumps(ret_dict)  
 
 @app.route('/count')
 @token_required(API_TOKEN,'txt') 
@@ -67,11 +83,22 @@ def get_counts():
     return str(conn.count())
 @app.route('/v1/count')
 @token_required(API_TOKEN,'json') 
-def get_counts_v1():
+def get_counts_v1(): 
+    score = request.args.get('abscore') 
+
+    if score == None:
+        score = MAX_SCORE 
+
+    score = int(score) 
+    if score > MAX_SCORE:
+        score = MAX_SCORE 
+    if score < MIN_SCORE:
+        score = MIN_SCORE 
+  
     conn = get_conn() 
     ret_dict = {}
     ret_dict['msg'] = 'ok'
-    ret_dict['count'] = conn.count()  
+    ret_dict['count'] = conn.count(score=score)  
     return json.dumps(ret_dict) 
 
 if __name__ == '__main__':
--- proxypool/db.py
+++ proxypool/db.py
@@ -29,14 +29,17 @@ class RedisClient(object):
         if not self.db.zscore(REDIS_KEY, proxy):
             return self.db.zadd(REDIS_KEY, score, proxy)
     
-    def random(self):
+    def random(self,num = 1):
         """
         随机获取有效代理，首先尝试获取最高分数代理，如果不存在，按照排名获取，否则异常
         :return: 随机代理
         """
         result = self.db.zrangebyscore(REDIS_KEY, MAX_SCORE, MAX_SCORE)
-        if len(result):
-            return choice(result)
+        if len(result): 
+            if num > 1:
+                return result[:num]
+            else:
+                return [choice(result)] 
         else:
             result = self.db.zrevrange(REDIS_KEY, 0, 100)
             if len(result):
@@ -75,12 +78,12 @@ class RedisClient(object):
         print('代理', proxy, '可用，设置为', MAX_SCORE)
         return self.db.zadd(REDIS_KEY, MAX_SCORE, proxy)
     
-    def count(self):
+    def count(self,score=MIN_SCORE):
         """
         获取数量
         :return: 数量
         """
-        return self.db.zcard(REDIS_KEY)
+        return len(self.db.zrangebyscore(REDIS_KEY, score, MAX_SCORE))
     
     def all(self):
         """
